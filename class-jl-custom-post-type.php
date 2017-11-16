eab71f0be5844c4ea136a7845267bdb72bd62d29
Jon Liebold <jliebold@outlook.com>
2017-11-16 13:45:01 -0500

    Added previously written pluralizer function

--- class-jl-custom-post-type.php
+++ class-jl-custom-post-type.php
@@ -55,6 +55,225 @@ if ( ! class_exists( 'JL_Custom_Post_Type' ) ) {
 
 		}
 
+		/** Pluralize Helper Function */
+		public static function pluralize( $string ) {
+			$exceptions = array(
+				'Amoyese',
+				'bison',
+				'Borghese',
+				'bream',
+				'breeches',
+				'britches',
+				'buffalo',
+				'cantus',
+				'carp',
+				'chassis',
+				'clippers',
+				'cod',
+				'coitus',
+				'Congoese',
+				'contretemps',
+				'corps',
+				'debris',
+				'diabetes',
+				'djinn',
+				'eland',
+				'elk',
+				'equipment',
+				'Faroese',
+				'flounder',
+				'Foochowese',
+				'Furniture',
+				'gallows',
+				'Genevese',
+				'Genoese',
+				'Gilbertese',
+				'graffiti',
+				'headquarters',
+				'herpes',
+				'hijinks',
+				'Hottentotese',
+				'information',
+				'innings',
+				'jackanapes',
+				'Kiplingese',
+				'Kongoese',
+				'Lucchese',
+				'Luggage',
+				'mackerel',
+				'Maltese',
+				'.*?media',
+				'mews',
+				'moose',
+				'mumps',
+				'Nankingese',
+				'news',
+				'nexus',
+				'Niasese',
+				'Pekingese',
+				'Piedmontese',
+				'pincers',
+				'Pistoiese',
+				'pliers',
+				'Portuguese',
+				'proceedings',
+				'rabies',
+				'rice',
+				'rhinoceros',
+				'salmon',
+				'Sarawakese',
+				'scissors',
+				'sea[- ]bass',
+				'series',
+				'Shavese',
+				'shears',
+				'siemens',
+				'species',
+				'staff',
+				'swine',
+				'testes',
+				'trousers',
+				'trout',
+				'tuna',
+				'Vermontese',
+				'Wenchowese',
+				'whiting',
+				'wildebeest',
+				'Yengeese',
+			);
+
+
+			$rules = array(
+				'/(s)tatus$/i'                                                           => '\1\2tatuses',
+				'/(quiz)$/i'                                                             => '\1zes',
+				'/^(ox)$/i'                                                              => '\1\2en',
+				'/([m|l])ouse$/i'                                                        => '\1ice',
+				'/(matr|vert|ind)(ix|ex)$/i'                                             => '\1ices',
+				'/(x|ch|ss|sh)$/i'                                                       => '\1es',
+				'/([^aeiouy]|qu)y$/i'                                                    => '\1ies',
+				'/(hive|gulf)$/i'                                                        => '\1s',
+				'/(?:([^f])fe|([lr])f)$/i'                                               => '\1\2ves',
+				'/sis$/i'                                                                => 'ses',
+				'/([ti])um$/i'                                                           => '\1a',
+				'/(p)erson$/i'                                                           => '\1eople',
+				'/(m)an$/i'                                                              => '\1en',
+				'/(c)hild$/i'                                                            => '\1hildren',
+				'/(f)oot$/i'                                                             => '\1eet',
+				'/(buffal|her|potat|tomat|volcan)o$/i'                                   => '\1\2oes',
+				'/(alumn|bacill|cact|foc|fung|nucle|radi|stimul|syllab|termin|vir)us$/i' => '\1i',
+				'/us$/i'                                                                 => 'uses',
+				'/(alias)$/i'                                                            => '\1es',
+				'/(analys|ax|cris|test|thes)is$/i'                                       => '\1es',
+				'/s$/'                                                                   => 's',
+				'/^$/'                                                                   => '',
+				'/$/'                                                                    => 's',
+			);
+
+			$uninflected = array(
+				'/.*[nrlm]ese/',
+				'/.*deer/',
+				'/.*fish/',
+				'/.*measles/',
+				'/.*ois/',
+				'/.*pox/',
+				'/.*sheep/',
+				'/people/',
+				'/cookie/',
+				'/police/',
+			);
+
+			$irregular = array(
+				'atlas'        => 'atlases',
+				'axe'          => 'axes',
+				'beef'         => 'beefs',
+				'brother'      => 'brothers',
+				'cafe'         => 'cafes',
+				'chateau'      => 'chateaux',
+				'niveau'       => 'niveaux',
+				'child'        => 'children',
+				'cookie'       => 'cookies',
+				'corpus'       => 'corpuses',
+				'cow'          => 'cows',
+				'criterion'    => 'criteria',
+				'curriculum'   => 'curricula',
+				'demo'         => 'demos',
+				'domino'       => 'dominoes',
+				'echo'         => 'echoes',
+				'foot'         => 'feet',
+				'fungus'       => 'fungi',
+				'ganglion'     => 'ganglions',
+				'genie'        => 'genies',
+				'genus'        => 'genera',
+				'graffito'     => 'graffiti',
+				'hippopotamus' => 'hippopotami',
+				'hoof'         => 'hoofs',
+				'human'        => 'humans',
+				'iris'         => 'irises',
+				'larva'        => 'larvae',
+				'leaf'         => 'leaves',
+				'loaf'         => 'loaves',
+				'man'          => 'men',
+				'medium'       => 'media',
+				'memorandum'   => 'memoranda',
+				'money'        => 'monies',
+				'mongoose'     => 'mongooses',
+				'motto'        => 'mottoes',
+				'move'         => 'moves',
+				'mythos'       => 'mythoi',
+				'niche'        => 'niches',
+				'nucleus'      => 'nuclei',
+				'numen'        => 'numina',
+				'occiput'      => 'occiputs',
+				'octopus'      => 'octopuses',
+				'opus'         => 'opuses',
+				'ox'           => 'oxen',
+				'passerby'     => 'passersby',
+				'penis'        => 'penises',
+				'person'       => 'people',
+				'plateau'      => 'plateaux',
+				'runner-up'    => 'runners-up',
+				'sex'          => 'sexes',
+				'soliloquy'    => 'soliloquies',
+				'son-in-law'   => 'sons-in-law',
+				'syllabus'     => 'syllabi',
+				'testis'       => 'testes',
+				'thief'        => 'thieves',
+				'tooth'        => 'teeth',
+				'tornado'      => 'tornadoes',
+				'trilby'       => 'trilbys',
+				'turf'         => 'turfs',
+				'volcano'      => 'volcanoes',
+			);
+
+			foreach ( $uninflected as $pattern ) {
+				if ( preg_match( $pattern, strtolower( $string ) ) ) {
+					$output = $string;
+
+					return $output;
+				}
+			}
+
+			if ( in_array( $string, $exceptions ) ) {
+				$output = $string;
+
+				return $output;
+			} elseif ( array_key_exists( $string, $irregular ) ) {
+				$output = $irregular[ $string ];
+
+				return $output;
+			} else {
+				foreach ( $rules as $rule => $pattern ) {
+					if ( preg_match( $rule, $string ) ) {
+						$output = preg_replace( $rule, $pattern, $string );
+
+						return $output;
+						break;
+					}
+
+				}
+			}
+		}
+
 	} // End Class.
 
 }// End if().
