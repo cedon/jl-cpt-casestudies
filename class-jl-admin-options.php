8408f1583c65c971c2de565ac7728434f7ea692e
Jon Liebold <jliebold@outlook.com>
2018-01-10 12:53:49 -0500

    Started building constructor for admin-options class

--- class-jl-admin-options.php
+++ class-jl-admin-options.php
@@ -16,14 +16,58 @@ if ( ! class_exists( 'JL_CustomAdminOptions' ) ) {
 	class JL_CustomAdminOptions {
 
 		/**
-		 * The name of the page that will be added to the WordPress admin panel
+		 * The name of the menu that will be used within the page's title element
 		 *
 		 * @since 1.0.0
 		 * @access public
 		 * @var string
 		 */
-		public $admin_page_name;
+		public $admin_page_title;
 
+		/**
+		 * The name of the menu that will appear in the admin menu
+		 *
+		 * @since 1.0.0
+		 * @access public
+		 * @var string
+		 */
+		public $admin_menu_title;
+
+		public function __construct( $title, $menu ) {
+
+			// Set Variables
+			$this->admin_page_title = self::beautify( $title );
+			$this->admin_menu_title = self::beautify( $menu );
+		}
+
+
+		/**
+		 * Changes a string like 'my_string' to 'My String' for display purposes
+		 *
+		 * @since 1.0
+		 * @access public
+		 *
+		 * @param string $string The string of text to beautify
+		 *
+		 * @return string The beautified text string
+		 */
+		public static function beautify( $string ) {
+			return ucwords( str_replace( '_', ' ', $string ) );
+		}
+
+		/**
+		 * Changes a string like 'My String' to 'my_string' for display purposes
+		 *
+		 * @since 1.0
+		 * @access public
+		 *
+		 * @param string $string The string of text to uglify
+		 *
+		 * @return string The uglified text string
+		 */
+		public static function uglify( $string ) {
+			return strtolower( str_replace( ' ', '_', $string ) );
+		}
 
 	} // End Class
 
